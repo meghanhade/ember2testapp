c0cbd3fd93ffed7ac071b9e641ac4f2bbdbaa6d4
Meghan <mmhade@gmail.com>
2015-08-10 18:33:32 -0700

    connect to model data

--- app/user/model.js
+++ app/user/model.js
@@ -4,7 +4,10 @@ var User = DS.Model.extend({
   username: DS.attr('string'),
   numberOfChirps: DS.attr('number'),
   numberOfFollowing: DS.attr('number'),
-  numberOfFollowers: DS.attr('number')
+  numberOfFollowers: DS.attr('number'),
+  aboutMe: DS.attr('string'),
+  joinedAt: DS.attr('date'),
+  chirps: DS.hasMany('chirps', { async: true })
 });
 
 User.reopenClass({
@@ -14,7 +17,10 @@ User.reopenClass({
       username: 't4t5', // Put your username here
       numberOfChirps: 2,
       numberOfFollowing: 5,
-      numberOfFollowers: 5
+      numberOfFollowers: 5,
+      aboutMe: 'I like making stuff.',
+      joinedAt: new Date('2015-06-08T09:30:26'),
+      chirps: [1, 2]
     }
   ]
 });
